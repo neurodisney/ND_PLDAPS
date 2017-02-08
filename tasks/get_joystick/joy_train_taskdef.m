f488638dfab0e3e509a6314948ce28dab3662156
Rig1 User <rig1_user@018>
2017-02-07 22:57:46 -0600

    WZ: Train Tue Feb 07 2017

--- tasks/get_joystick/joy_train_taskdef.m
+++ tasks/get_joystick/joy_train_taskdef.m
@@ -23,8 +23,8 @@ p.trial.task.Reward.Pull    = 0;             % If 1 then give reward for pulling
 p.trial.task.Reward.PullRew = 0.1;           % reward amount for pulling joystick (if p.trial.task.Reward.Pull == 1)
 
 p.trial.task.Reward.IncrConsecutive = 1;     % increase reward for subsequent correct trials. Otherwise reward will increase with the number of hits
-p.trial.task.Reward.Dur  = [0.4, 0.60, 0.8]; % reward duration [s], user vector to specify values used for incremental reward scheme
-p.trial.task.Reward.Step = [1, 2, 3];        % define the number of trials when to increase reward. CVector length can not be longer than p.trial.task.Reward.Dur
+p.trial.task.Reward.Dur  = [0.25, 0.50, 0.75]; % reward duration [s], user vector to specify values used for incremental reward scheme
+p.trial.task.Reward.Step = [1, 3, 5];        % define the number of trials when to increase reward. CVector length can not be longer than p.trial.task.Reward.Dur
 
 p.trial.task.Reward.Lag    = 0.10;           % Delay between response and reward onset
 p.trial.task.Reward.ManDur = 120;            % reward duration [s] for reward given by keyboard presses
