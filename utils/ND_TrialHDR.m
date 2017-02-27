027bd1cc384b9e0426a799ae8dfa348dfe147b5b
Rig1 User <rig1_user@018>
2017-02-26 23:46:44 -0600

    WZ: debug strobe and ttl, creating trial header

---
+++ utils/ND_TrialHDR.m
@@ -0,0 +1,25 @@
+function ND_TrialHDR(p)
+% encode information about a trial in a header format that is a chunk of
+% fixed length for an experiment.
+%
+% TODO: - implement robust method to encode relevant task parameters
+%       - initialize to store information in the pldaps data file or
+%         additional text file about the header format to allow for
+%         easy/robust reconstruction
+%
+%
+% wolf zinke, Feb 2017
+
+
+pds.tdt.strobe(p.trial.event.TRIAL_HDR_ON);  % encode begin of header/tail
+
+% default information 
+pds.tdt.strobe(p.trial.pldaps.iTrial);       % trial number
+pds.tdt.strobe(p.trial.Nr);                  % condition number
+pds.tdt.strobe(p.trial.outcome.CurrOutcome); % outcome
+
+% task dependent information 
+
+pds.tdt.strobe(p.trial.event.TRIAL_HDR_OFF);  % encode end of header/tail
+
+
