6a339f1e6501e96da8f9fec283829412f94328b9
zinke <wolfzinke@gmail.com>
2017-03-01 22:30:58 -0600

    WZ: TTL adjustment

--- utils/ND_TTLout.m
+++ utils/ND_TTLout.m
@@ -1,8 +1,8 @@
 function timings = ND_TTLout(chan, EV)
 % sends a TTL pulse over one of the last 8 bits of the digital output
-% of the DataPixx DB25 connector. If a event code is provided as second 
-% argument an additional code is sent via the first 16 bits (check  
-% ND_TDTstrobe for details about the 16 bit code). 
+% of the DataPixx DB25 connector. If a event code is provided as second
+% argument an additional code is sent via the first 16 bits (check
+% ND_TDTstrobe for details about the 16 bit code).
 %
 %    chan   pin
 %      1  -   9
@@ -23,7 +23,7 @@ if(nargin > 1)
     evntmask = dec2bin(EV,16);
 else
     % nothing to say
-    evntmask = repmat('0',1,16);
+    evntmask = '0000000000000000'; % hard code for efficiency % repmat('0',1,16);
 end
 
 % select channels for the DIO TTL pulse
@@ -35,9 +35,9 @@ chanmask = sprintf('%d',flip(chanmask));
 chanmask = bin2dec([chanmask, evntmask]); % use full 24 bits
 
 if(nargout == 0)
-    % just send TTL pulse and event code for efficiency 
+    % just send TTL pulse and event code for efficiency
     Datapixx('SetDoutValues', chanmask);
-    Datapixx('RegWrRd');   
+    Datapixx('RegWrRd');
 else
     % get time stamp back, might impair performance
     t = nan(2,1);
