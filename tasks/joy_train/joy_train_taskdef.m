a4a14d8d5c42feaa85eab91a9c661248a7eb0359
wolf zinke <wzinke@users.noreply.github.com>
2017-03-06 09:51:27 -0600

    Update joy_train_taskdef.m
    update task definition

--- tasks/joy_train/joy_train_taskdef.m
+++ tasks/joy_train/joy_train_taskdef.m
@@ -1,4 +1,4 @@
-% function p = joy_train_taskdef(p)
+function p = joy_train_taskdef(p)
 % define task parameters for the joystick training task.
 % This function will be executed at every trial start, hence it is possible
 % to edit it while the experiment is in progress in order to apply online
