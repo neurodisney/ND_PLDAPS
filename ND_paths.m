b38ddee033771ac6d527dbb562f20f7b1b594109
Nate Faber <nathaniel@njfaber.com>
2017-03-07 07:45:18 -0600

    NF: Make ND_paths behave in a relative instead of absolute way

--- ND_paths.m
+++ ND_paths.m
@@ -4,15 +4,18 @@
 %% Add PLDAPS paths to matlab
 loadPLDAPS;  % make sure file exists, otherwise modify from PLDAPS template and copy to a known location, e.g. ~/Documents/MATLAB/
 
+%% Get the directory where this script resides to add the paths relatively
+[pathStr,~,~] = fileparts(mfilename('fullpath'));
+
 %% general paths required to run the code
-addpath('~/Experiments/ND_PLDAPS/utils');
-addpath('~/Experiments/ND_PLDAPS/grfx');
-addpath('~/Experiments/ND_PLDAPS/trial_routines');
-addpath('~/Experiments/ND_PLDAPS/misc');
-addpath('~/Experiments/ND_PLDAPS/defaults');
-addpath('~/Experiments/ND_PLDAPS/flow');
+addpath(fullfile(pathStr,'utils'));
+addpath(fullfile(pathStr,'grfx'));
+addpath(fullfile(pathStr,'trial_routines'));
+addpath(fullfile(pathStr,'misc'));
+addpath(fullfile(pathStr,'defaults'));
+addpath(fullfile(pathStr,'flow'));
 
 
 %% task specific sub-directories
-addpath('~/Experiments/ND_PLDAPS/tasks/get_joy');
-addpath('~/Experiments/ND_PLDAPS/tasks/joy_train');
+addpath(fullfile(pathStr,'tasks/get_joy'));
+addpath(fullfile(pathStr,'tasks/joy_train'));
