5941f12c451379aaad205ca849df2f2d718ff8c3
NeuroDisney <none>
2017-03-21 14:37:49 -0500

    NF: Make function pds.audio.stopAll to stop any ongoing sounds

--- +pds/+audio/stopAll.m
+++ +pds/+audio/stopAll.m
@@ -2,9 +2,30 @@ function stopAll(p)
 %pds.audio.stopAll(p)    stops audio output off all sound files on both
 %PsychAudio and Datapixx
 
-if p.trial.sound.use && p.trial.sound.usePsychPortAudio && isfield(p.trial.sound, 'wavfiles')
-    fn=fieldnames(p.trial.sound.wavfiles);
-    for iFile = 1:length(fn)
-        PsychPortAudio('Stop', p.trial.sound.(fn{iFile}));
+if p.trial.sound.use
+    
+    %% Stop Datapixx Audio
+    if p.trial.sound.useDatapixxAudio
+        Datapixx('StopAudioSchedule')
+    end
+    
+    %% Stop PsychPortAudio
+    % Iterate through all the subfields of p.trial.sound
+    if p.trial.sound.usePsychPortAudio
+        fields = fieldnames(p.trial.sound);
+        for soundName=fields'
+            
+            soundStruct = p.trial.sound.(soundName{1});
+            
+            % Test if field is actually a sound by testing if it has the
+            % pahandle field. Otherwise it is variable or setting, like
+            % .datapixxVolume
+            if isfield(soundStruct,'paBoth')
+                % Stop the sounds playing on the three pa channels (left, right, and both)
+                PsychPortAudio('Stop',soundStruct.paLeft)
+                PsychPortAudio('Stop',soundStruct.paRight)
+                PsychPortAudio('Stop',soundStruct.paBoth)
+            end
+        end
     end
 end
