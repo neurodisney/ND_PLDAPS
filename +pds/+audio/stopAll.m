b6d95bbe8c8fc45de0e611957f584e239aeea6d9
NeuroDisney <none>
2017-03-21 16:41:36 -0500

    NF: Fix bug in audio/stopAll.m

--- +pds/+audio/stopAll.m
+++ +pds/+audio/stopAll.m
@@ -5,15 +5,15 @@ function stopAll(p)
 if p.trial.sound.use
     
     %% Stop Datapixx Audio
-    if p.trial.sound.useDatapixxAudio
-        Datapixx('StopAudioSchedule')
+    if p.trial.sound.useDatapixx
+        Datapixx('StopAudioSchedule');
     end
     
     %% Stop PsychPortAudio
     % Iterate through all the subfields of p.trial.sound
     if p.trial.sound.usePsychPortAudio
         fields = fieldnames(p.trial.sound);
-        for soundName=fields'
+        for soundName=fields';
             
             soundStruct = p.trial.sound.(soundName{1});
             
@@ -22,9 +22,9 @@ if p.trial.sound.use
             % .datapixxVolume
             if isfield(soundStruct,'paBoth')
                 % Stop the sounds playing on the three pa channels (left, right, and both)
-                PsychPortAudio('Stop',soundStruct.paLeft)
-                PsychPortAudio('Stop',soundStruct.paRight)
-                PsychPortAudio('Stop',soundStruct.paBoth)
+                PsychPortAudio('Stop',soundStruct.paLeft);
+                PsychPortAudio('Stop',soundStruct.paRight);
+                PsychPortAudio('Stop',soundStruct.paBoth);
             end
         end
     end
