1257c1927909f51d1d27ce8ef6cf1d0ff10511e1
NeuroDisney <none>
2017-03-24 14:59:02 -0500

    NF: Report newly pressed/released mouse buttons in addition to the current button state

--- utils/ND_CheckMouse.m
+++ utils/ND_CheckMouse.m
@@ -1,4 +1,4 @@
-function qp = ND_CheckMouse(p)
+function p = ND_CheckMouse(p)
 %% read in mouse information
 % code based on pldap's default trial function.
 % check for mouse actions and act accordingly.
@@ -6,13 +6,25 @@ function qp = ND_CheckMouse(p)
 %
 % wolf zinke, Feb 2017
 
-[cursorX, cursorY,isMouseButtonDown] = GetMouse();
+[cursorX, cursorY,buttons] = GetMouse();
 
-p.trial.mouse.samples =    p.trial.mouse.samples+1;
-p.trial.mouse.samplesTimes(p.trial.mouse.samples) = GetSecs;
-p.trial.mouse.X(p.trial.mouse.samples) = cursorX;
-p.trial.mouse.Y(p.trial.mouse.samples) = cursorY;
-p.trial.mouse.buttonPressSamples( :, p.trial.mouse.samples) = isMouseButtonDown';
+iSamples =    p.trial.mouse.samples+1;
+p.trial.mouse.samples = iSamples;
+p.trial.mouse.samplesTimes(iSamples) = GetSecs;
+p.trial.mouse.X(iSamples) = cursorX;
+p.trial.mouse.Y(iSamples) = cursorY;
+
+%% Process Mouse buttons
+
+% First get the state of the current buttons, and store it in the history
+p.trial.mouse.buttons = buttons;
+p.trial.mouse.buttonPressSamples( :, iSamples) = buttons';
+
+% Then, check whether the button being down is new for this frame
+% Store this in mouse.newButtons. 1 = newly pressed, 0 = no change, -1 = newly released
+lastButtons = p.trial.mouse.buttonPressSamples(:, iSamples - 1);
+p.trial.mouse.newButtons = buttons' - lastButtons;
+
 
 %if(p.trial.mouse.useAsEyepos)
 %    if(p.trial.pldaps.eyeposMovAv == 1) % just take a single sample
