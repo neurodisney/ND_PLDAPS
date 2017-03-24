b1ad9006babb584e91f74dc7ef5e515f68ed8d2e
Rig1 User <rig1_user@018>
2017-03-23 19:14:59 -0500

    WZ: encode time stamp in trial header.

--- utils/ND_TrialHDR.m
+++ utils/ND_TrialHDR.m
@@ -15,13 +15,20 @@ pds.tdt.strobe(p.trial.event.TRIAL_HDR_ON);
 
 % ------------------------------------------------------------------------%
 %% default information 
-pds.tdt.strobe(p.trial.pldaps.iTrial);       % trial number
+% this should be minimla information that is valid/applicable for all tasks.
 
-% TODO?
-%  strobe hour
-%  strobe min
-%  strobe sec
-%  strobe ms
+pds.tdt.strobe(p.trial.pldaps.iTrial);  % trial number
+
+% get time stamp as encoded for trial start
+cpos = find(p.trial.TrialStart, ':');
+if(length(cpos) < 3)
+    error('Something unexpected happened with the time string for trial start!')
+else
+    pds.tdt.strobe(str2num(p.trial.TrialStart(        1:cpos(1)-1)));   % hour
+    pds.tdt.strobe(str2num(p.trial.TrialStart(cpos(1)+1:cpos(2)-1)));   % minutes
+    pds.tdt.strobe(str2num(p.trial.TrialStart(cpos(2)+1:cpos(3)-1)));   % seconds
+    pds.tdt.strobe(str2num(p.trial.TrialStart(cpos(3)+1:end)));         % milliseconds
+end
 
 pds.tdt.strobe(p.trial.Nr);                  % condition number
 pds.tdt.strobe(p.trial.outcome.CurrOutcome); % outcome
