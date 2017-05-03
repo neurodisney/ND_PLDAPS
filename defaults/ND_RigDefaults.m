f3da38374445c4bb045e85022fa6d9db646a3549
Rig1 User <rig1_user@018>
2017-05-03 10:24:00 -0500

    NF: Make screen size accruate

--- defaults/ND_RigDefaults.m
+++ defaults/ND_RigDefaults.m
@@ -49,9 +49,9 @@ SS.datapixx.adc.channelMapping                  = {};     % Specify where to sto
 %% Display settings: specify options for the screen.
 SS.display.bgColor                              = [0.25, 0.25, 0.25];  % datapixx background color. This is the base color datapix uses a screen color and has to be monochrome. It can be changed during trial.
 SS.display.scrnNum                              = 1;      % screen number for full screen display, 1 is monkey-screen,0 is experimenter screen
-SS.display.viewdist                             = 114;    % screen distance to the observer                            !!!
-SS.display.heightcm                             = 35;     % height of the visible screen in cm                         !!!
-SS.display.widthcm                              = 64;     % width  of the visible screen in cm                         !!!
+SS.display.viewdist                             = 114;    % screen distance to the observer
+SS.display.heightcm                             = 40;     % height of the visible screen in cm
+SS.display.widthcm                              = 71;     % width  of the visible screen in cm
 SS.display.screenSize                           = [];     % size of the window to create pixels in, leave empty for full screen
 
 SS.display.useOverlay                           = 1;      % create an overlay pointer
