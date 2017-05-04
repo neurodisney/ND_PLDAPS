4bf65861e33d5b6a83566ee21b1d41d563d44e79
Nate Faber <nathaniel@njfaber.com>
2017-05-04 09:05:28 -0500

    NF: Remove calibration colors from FixTrain

--- tasks/NHP_training/FixTrain/FixTrain.m
+++ tasks/NHP_training/FixTrain/FixTrain.m
@@ -49,17 +49,6 @@ if(isempty(state))
 
     p.trial.task.Color_list = Shuffle({'Fix_W', 'Fix_R', 'Fix_G', 'Fix_B', 'Fix_O', 'Fix_Y', 'Fix_C', 'Fix_M'});  
     
-    
-    % Colors used for calibration points
-    bgColor = p.trial.display.bgColor; % For making things invisible on the monkey screen;
-    ND_DefineCol(p, 'Calib_LG', 70, [0.69, 1.00, 0.69], bgColor); % Light Green
-    ND_DefineCol(p, 'Calib_G',  71, [0.00, 1.00, 0.00], bgColor); % Green
-    ND_DefineCol(p, 'Calib_DG', 72, [0.00, 0.69, 0.00], bgColor); % Dark Green
-    ND_DefineCol(p, 'Calib_LR', 73, [1.00, 0.69, 0.69], bgColor); % Light Red
-    ND_DefineCol(p, 'Calib_R',  74, [1.00, 0.00, 0.00], bgColor); % Red
-    ND_DefineCol(p, 'Calib_DR', 75, [0.69, 0.00, 0.00], bgColor); % Dark Red
-    ND_DefineCol(p, 'Calib_Y',  76, [1.00, 1.00, 0.00], bgColor); % Yellow
-    
     % --------------------------------------------------------------------%
     %% Enable random positions
     p.trial.task.RandomPos = 0;
