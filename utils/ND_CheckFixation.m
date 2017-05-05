1561b238c0d88c187fd0ff3b27e52366e4e1c1b4
Nate Faber <nathaniel@njfaber.com>
2017-05-05 14:25:22 -0500

    NF: Clean up ND_Checkfixation a little

--- utils/ND_CheckFixation.m
+++ utils/ND_CheckFixation.m
@@ -10,10 +10,10 @@ function p = ND_CheckFixation(p)
 % TODO: Define sample based on a time period not number of samples.
 %
 % wolf zinke, Jan. 2017
+% Nate Faber, May 2017
 
 %% get eye position data
 if(p.trial.mouse.useAsEyepos)
-    nSamples = p.trial.behavior.fixation.Sample;
     
     % Get the last mouse position
     mousePos = p.trial.mouse.cursorSamples(:,end);   
@@ -22,12 +22,12 @@ if(p.trial.mouse.useAsEyepos)
     p.trial.eyeY = mousePos(2);
     
 else
+    % Avoid taking too many samples during the first trial by accident
+    sIdx = min(p.trial.datapixx.adc.dataSampleCount,p.trial.behavior.fixation.Sample) - 1;
     
-    sIdx = (p.trial.datapixx.adc.dataSampleCount - p.trial.behavior.fixation.Sample + 1) : p.trial.datapixx.adc.dataSampleCount;  % determine the position of the sample. If this causes problems with negative values in the first trial, make sure to use only positive indices.
-
     % calculate a moving average of the eye position for display reasons
-    p.trial.eyeX   = p.trial.eyeCalib.gain(1) * (prctile(p.trial.AI.Eye.X(sIdx), 50) - p.trial.eyeCalib.offset(1));
-    p.trial.eyeY   = p.trial.eyeCalib.gain(2) * (prctile(p.trial.AI.Eye.Y(sIdx), 50) - p.trial.eyeCalib.offset(2));
+    p.trial.eyeX   = p.trial.eyeCalib.gain(1) * (prctile(p.trial.AI.Eye.X(end-sIdx:end), 50) - p.trial.eyeCalib.offset(1));
+    p.trial.eyeY   = p.trial.eyeCalib.gain(2) * (prctile(p.trial.AI.Eye.Y(end-sIdx:end), 50) - p.trial.eyeCalib.offset(2));
 end
 
 p.trial.eyeAmp = sqrt((p.trial.behavior.fixation.fixPos(1) - p.trial.eyeX)^2 + ...
