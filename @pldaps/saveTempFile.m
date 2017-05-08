4ca28dbb2f1b7d180910a4ffaedd371a9d2670a4
zinke <wolfzinke@gmail.com>
2017-05-08 12:29:59 -0500

    WZ: removed eval call when saving temporary trial data

--- @pldaps/saveTempFile.m
+++ @pldaps/saveTempFile.m
@@ -6,12 +6,13 @@ result= [];
 
 if(~p.trial.pldaps.nosave && p.trial.pldaps.save.trialTempfiles)
     
-	evalc(['trial' num2str(p.trial.pldaps.iTrial) '= p.trial']);
-        
+	%evalc(['trial' num2str(p.trial.pldaps.iTrial) '= p.trial']);
+    
     try  
+        ctrial = p.trial;    
         save(fullfile(p.defaultParameters.session.tmpdir, ...
                      [p.defaultParameters.session.filestem, '_T', num2str(p.trial.pldaps.iTrial, '%.5d'), '.pds']),  ...
-                     ['trial', num2str(p.trial.pldaps.iTrial)]);
+                     '-struct', 'ctrial');
     catch result
          warning('pldaps:saveTempFile','Failed to save temp file in %s', p.defaultParameters.session.tmpdir)
     end
