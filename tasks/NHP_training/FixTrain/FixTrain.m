da85dea09799d0f3f779c2962b6f8fe88ea31050
Nate Faber <nathaniel@njfaber.com>
2017-05-10 09:35:42 -0500

    NF: Set up today's conditions

--- tasks/NHP_training/FixTrain/FixTrain.m
+++ tasks/NHP_training/FixTrain/FixTrain.m
@@ -102,30 +102,41 @@ if(isempty(state))
     c4.reward.MinWaitInitial = 0.73;
     c4.reward.MaxWaitInitial = 0.77;
     c4.reward.nRewards       = [1    8   ];
-    c4.reward.Dur            = [0.2  0.2 ]; 
-    c4.reward.Period         = [0.75 0.75];   
+    c4.reward.Dur            = [0.10 0.10]; 
+    c4.reward.Period         = [1.00 1.00];   
     c4.reward.jackpotDur     = 0.5;
-    c4.nTrials = 300;
+    c4.nTrials = 50;
     
     % condition 5
     c5.Nr = 5;
     c5.reward.MinWaitInitial = 0.98;
     c5.reward.MaxWaitInitial = 1.02;
-    c5.reward.nRewards       = [1    8   ];
-    c5.reward.Dur            = [0.20 0.20];
-    c5.reward.Period         = [0.50 0.50];   
+    c5.reward.nRewards       = [1    14  ];
+    c5.reward.Dur            = [0.10 0.10];
+    c5.reward.Period         = [0.75 0.75];   
     c5.reward.jackpotDur     = 0.5;
-    c5.nTrials = 300;
+    c5.nTrials = 100;
     
     % condition 6
-    c6.Nr = 5;
+    c6.Nr = 6;
     c6.reward.MinWaitInitial = 1.23;
     c6.reward.MaxWaitInitial = 1.27;
-    c6.reward.nRewards       = [1    8   ];
-    c6.reward.Dur            = [0.25 0.25];
-    c6.reward.Period         = [0.40 0.40];   
+    c6.reward.nRewards       = [1    18  ];
+    c6.reward.Dur            = [0.15 0.15];
+    c6.reward.Period         = [0.60 0.60];   
     c6.reward.jackpotDur     = 0.5;
     c6.nTrials = 300;
+    
+    % condition 7
+    c6.Nr = 7;
+    c6.reward.MinWaitInitial = 1.48;
+    c6.reward.MaxWaitInitial = 1.52;
+    c6.reward.nRewards       = [1    25  ];
+    c6.reward.Dur            = [0.20 0.20];
+    c6.reward.Period         = [0.30 0.30];   
+    c6.reward.jackpotDur     = 1.0;
+    c6.nTrials = 1000;
+    
     
     % Fill a conditions list with n of each kind of condition sequentially
     conditions = cell(1,5000);
@@ -133,7 +144,7 @@ if(isempty(state))
     totalTrials = 0;
     
     % Iterate through each condition to fill conditions
-    conditionsIterator = {c3,c4,c5,c6};
+    conditionsIterator = {c4,c5,c6};
     
     for iCond = 1:size(conditionsIterator,2)
         cond = conditionsIterator(iCond);
