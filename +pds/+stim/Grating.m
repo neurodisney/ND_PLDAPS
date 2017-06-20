a495e6a783464b93091a5dc1ed0b3c949b810b25
Nate Faber <nathaniel@njfaber.com>
2017-06-20 14:41:19 -0500

    NF: Do not use procedural generating grating, make it locally instead

--- +pds/+stim/Grating.m
+++ +pds/+stim/Grating.m
@@ -4,7 +4,7 @@ classdef Grating
 % within the
 
 % Variables that can be changed about the sine wave grating after it has been created:
-% Spatial Frequency
+
 % Temporal Frequency
 % Orientation
 % Contrast
@@ -12,12 +12,13 @@ classdef Grating
 % Alpha
 %
 % Variables that can't be changed (for now)
+% Spatial Frequency
 % radius
 % Contrast Method
 % Background color offset
+% Resolution
 
 properties
-    sFreq
     tFreq
     angle
     contrast
@@ -26,16 +27,18 @@ properties
 end
 
 properties (SetAccess = private)
+    sFreq
     contrastMethod
     radius
     bgOffset
     gratingRect
+    res
 end
 
 properties (SetAccess = private, Hidden = true)
-    pcmult %Precontrast multiplier
     genTime %When the grating was created (to calculate drifting phase)
     texture % The actual texture used for drawing
+    pcmult
 end
 
 methods
@@ -43,7 +46,6 @@ methods
     function obj = Grating(p, radius)
         % Load variables
         % Changeable
-        obj.sFreq = p.trial.stim.grating.sFreq;
         obj.tFreq = p.trial.stim.grating.tFreq;
         obj.angle = p.trial.stim.grating.angle;
         obj.contrast = p.trial.stim.grating.contrast;
@@ -51,21 +53,23 @@ methods
         obj.alpha = p.trial.stim.grating.alpha;
         
         % Unchangeable after loading
+        obj.sFreq = p.trial.stim.grating.sFreq;
         obj.contrastMethod = p.trial.stim.grating.contrastMethod;        
         obj.radius = radius;
         obj.genTime = p.trial.CurTime;
+        obj.res = p.trial.stim.grating.res;
         
         switch obj.contrastMethod
             case 'raw'
-                obj.bgOffset = [0.5 0.5 0.5 0];
+                obj.bgOffset = 0.5;
                 obj.pcmult = 0.5;
                 
             case 'bgshift'
-                obj.bgOffset = [p.trial.display.bgColor, 0];
+                obj.bgOffset = p.trial.display.bgColor(1);
                 obj.pcmult = 0.5;
                 
             case 'bgscale'
-                obj.bgOffset = [p.trial.display.bgColor, 0];
+                obj.bgOffset = p.trial.display.bgColor(1);
                 obj.pcmult = 0.5 + abs(obj.bgOffset(1) - 0.5);
                 
             otherwise
@@ -74,14 +78,27 @@ methods
         
         window = p.trial.display.ptr;
         
-        % For some reason, all generated gratings are off by 0.5,0.5
-        % degrees. Maybe it has something to do with the coordinate
-        % transformations... Therefore, do this hack of fix.
-        width = 2*radius;
-        height = 2*radius;
-        % The texture that gets drawn
-        [obj.texture, obj.gratingRect] = CreateProceduralSineGrating(window, width, height, ...
-            obj.bgOffset, inf, obj.pcmult);
+        % Create a special texture drawing shader for masked texture drawing:
+        glsl = MakeTextureDrawShader(window, 'SeparateAlphaChannel');
+        
+        % Scale the spatial frequency to match the resolution of the grating
+        sFreqTex = obj.sFreq * obj.radius / obj.res;
+        
+        % Make sure that at least one complete cycle is created in the texture
+        %q = ceil(1/sFreqTex);
+        
+        % Create the texture matrix
+        x = meshgrid(-obj.res:obj.res, -obj.res:obj.res);
+        grating = obj.bgOffset + obj.pcmult * cos(sFreqTex*2*pi*x); 
+        
+        % Create a circular aperture using the separate alpha-channel:
+        [x,y] = meshgrid(-obj.res:obj.res, -obj.res:obj.res);
+        circle = (x.^2 + y.^2 <= obj.res^2);
+        grating(:,:,2) = 0;
+        grating(1:2*obj.res+1, 1:2*obj.res+1, 2) = circle;
+               
+        % Make the texture that gets drawn
+        obj.texture = Screen('MakeTexture', window, grating, [], [], 2, [], glsl);
 
     end
     
@@ -90,18 +107,19 @@ methods
         
         % Use the current time to calculate the phase for accurate temporal frequency
         elapsedTime = p.trial.CurTime - obj.genTime;
-        phase = 360 * elapsedTime * obj.tFreq;
+        sFreqTex = obj.sFreq * obj.radius / obj.res;
+        phaseOffset =  mod(elapsedTime * obj.tFreq,1) / sFreqTex;
         
         % Calculate the rect using the position
         destRect = [obj.pos - obj.radius, obj.pos + obj.radius];
         
         % Filter mode (not sure what the best value is yet)
         % For more information see the PTB documentation for Screen('DrawTexture')
-        filterMode = 0;
+        filterMode = [];
         
         % Draw the texture
         Screen('DrawTexture', window, obj.texture, [], destRect, obj.angle, filterMode, ...
-            obj.alpha, [], [], [], [phase, obj.sFreq, obj.contrast, 0]);
+            obj.contrast, [], [], [], [0, phaseOffset, 0, 0]);
         
     end
     
