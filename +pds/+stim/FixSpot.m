ec25329783f1d40cfc6fc34e09ae5f4abb14da94
Nate Faber <nathaniel@njfaber.com>
2017-07-03 11:56:11 -0500

    NF: Make FixSpot stimulus

---
+++ +pds/+stim/FixSpot.m
@@ -0,0 +1,59 @@
+classdef FixSpot < BaseStim
+
+properties
+    size
+    type
+    color
+end
+
+methods
+    
+    function obj = FixSpot(p,pos,size,type,color,fixWin)
+        if nargin < 6 || isempty(fixWin)
+            fixWin = p.trial.stim.fixspot.fixWin;
+        end
+        
+        if nargin < 5 || isempty(color)
+            color = p.trial.stim.fixspot.color;
+        end
+        
+        if nargin < 4 || isempty(type)
+            type = p.trial.stim.fixspot.type;
+        end
+        
+        if nargin < 3 || isempty(size)
+            size = p.trial.stim.fixspot.size;
+        end
+        
+        if nargin < 2 || isempty(pos)
+            pos = p.trial.stim.fixspot.pos;
+        end
+        
+        % Load the BaseStim superclass
+        obj@BaseStim(p, pos, fixWin)
+        
+        obj.color = color;
+        obj.type = type;
+        obj.size = size;
+        obj.pos = pos;
+        
+    end
+    
+    function draw(p)
+        switch  obj.type
+            case 'disc'
+                Screen('gluDisk', p.trial.display.overlayptr, p.trial.display.clut.(obj.color), ...
+                    obj.pos(1), obj.pos(2), obj.size);
+            case 'rect'
+                Screen('FillRect',  p.trial.display.overlayptr, p.trial.display.clut.(obj.color), ...
+                    [obj.pos - [obj.size obj.size]; obj.pos + [obj.size obj.size]]);
+                
+            otherwise
+                error('Unknown type of fixation spot: %s', p.trial.behavior.fixation.FixType);
+        end
+    end
+    
+end
+    
+end
+
