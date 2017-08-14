17cd426fdd6a0ab204154acd07a59add7196c32e
Nate Faber <nathaniel@njfaber.com>
2017-08-14 15:21:52 -0500

    NF: Copy over RevCorr to form the new basis of man_rf_map

---
+++ tasks/manual_rf_map/man_rf_map_taskdef.m
@@ -0,0 +1,123 @@
+function p = man_rf_map_taskdef(p)
+% define task parameters for the joystick training task.
+% This function will be executed at every trial start, hence it is possible
+% to edit it while the experiment is in progress in order to apply online
+% modifications of the task.
+%
+% TODO: - Make sure that changed parameters are kept in the data file, i.e.
+%         that there is some log when changes happened
+%       - read in only changes in order to allow quicker manipulations via the
+%         keyboard without overwriting it every time by calling this routine
+%
+%
+% wolf zinke, Dec. 2016
+
+% ------------------------------------------------------------------------%
+%% Condition/Block design
+p.trial.task.EqualCorrect = 0; % if set to one, trials within a block are repeated until the same number of correct trials is obtained for all conditions
+
+% ------------------------------------------------------------------------%
+%% Reward
+
+% manual reward from experimenter
+p.trial.reward.ManDur = 0.05;         % reward duration [s] for reward given by keyboard presses
+
+p.trial.reward.Dur    = 0.05;         % Reward given after each complete stim presentation
+
+p.trial.reward.jackpotDur = 0.3;     % Reward for holding fixation until jackpotTime
+p.trial.reward.IncrConsecutive = 1;  % use rewarding scheme that gives more rewards with subsequent correct trials
+p.trial.reward.nPulse          = 2;  % number of reward pulses
+p.trial.reward.PulseStep       = [2, 3, 4, 5]; % increase number of pulses with this trial number
+
+% ------------------------------------------------------------------------%
+%% Timing
+p.trial.task.Timing.WaitFix = 4;    % Time to wait for fixation before NoStart
+
+% Main trial timings
+p.trial.task.fixLatency       = 0.15; % Time to hold fixation before mapping begins
+
+
+p.trial.task.stimOnTime       = 0.1;   % How long each stimulus is presented
+p.trial.task.stimOffTime      = 0.1;     % Gaps between succesive stimuli
+
+p.trial.task.jackpotTime      = 4;     % How long stimuli are presented before trial ends and jackpot is given
+
+% inter-trial interval
+p.trial.task.Timing.MinITI  = 1.0;  % minimum time period [s] between subsequent trials
+p.trial.task.Timing.MaxITI  = 2.5;  % maximum time period [s] between subsequent trials
+
+% penalties
+p.trial.task.Timing.TimeOut =  0;   % Time [s] out for incorrect responses
+
+% ------------------------------------------------------------------------%
+%% RF mapping parameters
+p.trial.RF.spatialRes  = 25;  % Number sections to subdivide the possible visual space into
+p.trial.RF.coarse.temporalRange  = [-0.5, 0]; % Range from each spike to calculate the reverse correlation
+p.trial.RF.temporalRes = 25;  % Number of sections to divide the history into for analysis
+
+p.trial.RF.temporalProfileRefineProportion = 0.2;  % During the coarse mapping, a temproal profile of stims is created, this proporiton of the max is used to cut off the accepted times for the fine mapping
+
+p.trial.RF.maxSpikesPerTrial = 1000;   % Max spikes per trial, for preallocation purposes.
+
+p.trial.RF.channels = 4;  % Which channels of the incoming spike data to use for spikes
+p.trial.RF.sortCodes = 1;  % Which sort codes to use
+
+% ------------------------------------------------------------------------%
+%% fixation spot parameters
+p.trial.stim.FIXSPOT.pos    = [0,0];
+p.trial.stim.FIXSPOT.type   = 'disc';     % shape of fixation target, options implemented atm are 'disc' and 'rect', or 'off'
+p.trial.stim.FIXSPOT.color  = 'white';  % color of fixation spot (as defined in the lookup tables)
+p.trial.stim.FIXSPOT.size   = 0.15;        % size of the fixation spot
+p.trial.stim.FIXSPOT.fixWin = 2;
+
+% ------------------------------------------------------------------------%
+%% Grating stimuli parameters
+p.trial.stim.GRATING.res          = 300;
+p.trial.stim.GRATING.fixWin       = 0;
+
+% Will use a grid based layout to display the stimuli
+p.trial.stim.coarse.xRange = [-10, -2];
+p.trial.stim.coarse.yRange = [-10, -2];
+p.trial.stim.coarse.grdStp = 0.25;
+
+p.trial.stim.fine.extent = 2; % From the max value in the coarse mapping, extend out +- this far
+p.trial.stim.fine.grdStp = 0.1;
+
+
+% For the following parameters, an array can be specified and all possible combinations
+% will be tested.
+
+p.trial.stim.coarse.angle    = [0, 90];
+p.trial.stim.fine.angle      = [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5];
+
+p.trial.stim.coarse.radius   = 0.75;
+p.trial.stim.fine.radius     = 0.75;
+
+% spatial frequency (cycles per degree)
+p.trial.stim.coarse.sFreq    = 1.5;
+p.trial.stim.fine.sFreq      = 1.5;
+
+% temporal frequency (cycles per second, 0 is stationary)
+p.trial.stim.coarse.tFreq    = 0;
+p.trial.stim.fine.tFreq      = 0;
+
+% contrast
+p.trial.stim.coarse.contrast = 1;
+p.trial.stim.fine.contrast   = 1;
+
+% ------------------------------------------------------------------------%
+%% Fixation parameters
+
+p.trial.behavior.fixation.BreakTime = 0.050;  % minimum time [ms] to identify a fixation break
+p.trial.behavior.fixation.entryTime = 0.150;  % minimum time to stay within fixation window to detect initial fixation start
+
+% ------------------------------------------------------------------------%
+%% Task parameters
+p.trial.task.breakFixCheck = 0.2; % Time after a stimbreak where if task is marked early or stim break is calculated
+
+% ------------------------------------------------------------------------%
+%% Trial duration
+% maxTrialLength is used to pre-allocate memory at several initialization
+% steps. It specifies a duration in seconds.
+
+p.trial.pldaps.maxTrialLength = 15;
--- tasks/manual_rf_map/start_man_rf_map.m
+++ tasks/manual_rf_map/start_man_rf_map.m
@@ -3,9 +3,7 @@ function start_man_rf_map(subjname)
 % use this script to define a default configuration in order to create a
 % pldaps object and run it with the rf_map trial function.
 %
-% Nate Faber, Feb. 2017
-% Adapted from:
-% wolf zinke, Dec. 2016
+% Nate Faber, Aug. 2017
 
 
 % ------------------------------------------------------------------------%
@@ -16,68 +14,116 @@ if(~exist('subjname','var') || isempty(subjname))
     subjname = 'tst';
 end
 
-% function to set up experiment (and maybe also including trial function)
+% name of subject. This will be used to create a subdirectory with this name.
+if(~exist('rig','var') || isempty(rig))
+    [~, rigname] = system('hostname');
+    rig = str2num(rigname);
+end
+
+% name of subject. This will be used to create a subdirectory with this name.
+if(~exist('experimenter','var') || isempty(experimenter))
+    experimenter = getenv('USER');
+end
+
+%-------------------------------------------------------------------------%
+%% load default settings into a struct
+SS = ND_RigDefaults(rig);    % load default settings according to the current rig setup
+
+%% ################## Edit within the following block ################## %%
+
+%% Define task related functions
+
+% function to set up experiment (and maybe also including the trial function)
 exp_fun = 'man_rf_map';
 
-% ------------------------------------------------------------------------%
-%% load default settings into a struct
-SS = ND_RigDefaults;    % load default settings according to the current rig setup
-
 % define trial function (could be identical with the experimentSetupFile that is passed as argument to the pldaps call
-SS.pldaps.trialFunction = exp_fun; % This function is both, set-up for the experiment session as well as the trial function
+SS.pldaps.trialFunction = exp_fun;     % This function is both, set-up for the experiment session as well as the trial function
+SS.task.TaskDef    = 'man_rf_map_taskdef';  % function that provides task specific parameter definitions
+SS.task.AfterTrial = 'man_rf_map_aftertrial';  % function that provides runs task specific actions after a trial
+SS.plot.routine    = 'man_rf_map_plots';    % function for online plotting of session progress
 
 % ------------------------------------------------------------------------%
-%% make modifications of default settings
-SS.sound.use          = 1;
-SS.display.bgColor    = [0.5, 0.5, 0.5];
-
-% Use the mouse
-SS.mouse.use = 1;
-
-SS.pldaps.nosave = 1;  % For now do not bother with the pldaps file format, use plain text file instead.
-
-% For now turn off reward since not setup for rig2 where I am testing this.
-SS.datapixx.useForReward = 0;
-
-% Turn off joystick
-SS.datapixx.useJoystick = 0;
-
-% SS.datapixx.adc.channels                        = []; % List of channels to collect data from. Channel 3 is as default reserved for reward.               !!!
-% SS.datapixx.adc.channelMapping = {'AI.adc'}; % Specify where to store the collected data. WZ: Seems that the names need to start with 'datapixx.' to ensure that the fields are created (apparently only in the datapixx substructure).
-
-SS.sound.use                  = 0; % no sound for now
-SS.behavior.fixation.use      = 0; % eye position is behavioral relevant
+%% define variables that need to passed to next trial
+SS.editable = {'stim.count','stim.iStim','stim.iPos','stim.stage','stim.fine.xRange','stim.fine.yRange','RF.coarse','RF.fine','RF.flag_new','RF.flag_fine'};
+                  
+% ------------------------------------------------------------------------%
+%% Enable required components if needed
+% Most of the components are disabled as default. If needed for the task enable them here.
+SS.sound.use                  = 1;
+SS.behavior.fixation.use      = 1; % eye position is behavioral relevant
 SS.behavior.joystick.use      = 0; % joystick is behavioral relevant
 SS.plot.do_online             = 0; % run online data analysis between two subsequent trials
 SS.pldaps.nosave              = 0; % disable saving data to pds files
 SS.pldaps.draw.joystick.use   = 0; % draw joystick states on control screen
-SS.pldaps.draw.eyepos.use     = 0; % enable drawing of the eye position.
+SS.pldaps.draw.eyepos.use     = 1; % enable drawing of the eye position.
 SS.pldaps.draw.photodiode.use = 0; % enable drawing the photo diode square
-SS.datapixx.useForReward      = 0; % use datapixx analog output for reward
+SS.datapixx.useForReward      = 1; % use datapixx analog output for reward
 
-SS.pldaps.draw.grid.use       = 0;
+SS.pldaps.draw.grid.use       = 1;
 
-SS.datapixx.useAsEyepos       = 0;
+SS.datapixx.useAsEyepos       = 1;
 SS.datapixx.useJoystick       = 0;
 SS.datapixx.TTL_trialOn       = 0;
 
-SS.behavior.fixation.useCalibration = 0;
+SS.behavior.fixation.useCalibration = 1;
+SS.behavior.fixation.enableCalib = 0;
 
-SS.behavior.fixation.on = 0; % fixation.on for this task
+SS.behavior.fixation.on = 1; % fixation.on for this task
 
 SS.pldaps.GetTrialStateTimes  = 0; % for debugging, save times when trial states are called
 
 % ------------------------------------------------------------------------%
+%% make modifications of default settings
+% If there are modification from the default settings needed, copy the
+% needed lines from ND_RigDefaults and alter the values here.
+
+SS.display.bgColor    = [0.5, 0.5, 0.5];  % change background color
+SS.datapixx.adc.srate = 1000; % for a 1k tracker, less if you don’t plan to use it for offline use
+
+SS.behavior.fixation.FixWin     = 2.5;
+SS.behavior.fixation.FixGridStp = [3, 3]; % x,y coordinates in a 9pt grid
+SS.behavior.fixation.FixWinStp  = 0.5;    % change of the size of the fixation window upon key press
+
+%% ################## Edit within the preceding block ################### %%
+%% ### Do not change code below [unless you know what you are doing]! ### %%
+
+% ------------------------------------------------------------------------%
+%% Special debug mode variables
+if strcmp(subjname,'mouse')
+    
+    % Use the mouse as eyeposition
+    SS.mouse.use = 1;
+    SS.mouse.useAsEyepos = 1;
+    
+    % Don't collect any analog channels
+    SS.datapixx.adc.PupilChannel   = [];
+    SS.datapixx.adc.XEyeposChannel = [];
+    SS.datapixx.adc.YEyeposChannel = [];
+    %SS.datapixx.adc.RewardChannel  = [];  
+    SS.datapixx.useAsEyepos        = 0;
+    SS.behavior.joystick.use       = 0;
+    %SS.datapixx.useForReward       = 0;
+    %SS.sound.use                   = 0;
+    
+    % Do manual spiking using the s key
+    SS.tdt.use                    = 1;
+    
+end
+% ------------------------------------------------------------------------%
 %% create the pldaps class
 p = pldaps(subjname, SS, exp_fun);
 
+% keep rig/experimenter specific inforamtion
+p.defaultParameters.session.rig          = rig;
+p.defaultParameters.session.experimenter = experimenter;
 
 % ------------------------------------------------------------------------%
 %% run the experiment
-p.run
+p.run;
 
 % ------------------------------------------------------------------------%
 %% Ensure DataPixx is closed
 if(Datapixx('IsReady'))
     Datapixx('Close');
 end
+
