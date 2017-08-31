ede11dcdad1452784129455ebc2eb6aeffdcd721
Nate Faber <nathaniel@njfaber.com>
2017-08-31 18:09:01 -0500

    NF: Reorganize plots file to use functions

--- tasks/revcorr_rfmap/RevCorr_plots.m
+++ tasks/revcorr_rfmap/RevCorr_plots.m
@@ -29,148 +29,188 @@ try
     % Clear the figure window
     clf;
    
-    %% Plot of fixation time
-    % Takes up the top row of the plot. Shows fixation time over the duration of the entire experiment
-    subplot(plotRows, plotCols, 1:plotCols-1)
     
-    % Get and save relavent information
-        % Trial times relative to start of experiment
-    p.plotdata.TaskStart(nTrials) = p.trial.EV.TaskStart;
-    p.plotdata.times(nTrials) = (p.trial.EV.TaskStart - p.plotdata.TaskStart(1)) / 60;
-    times = p.plotdata.times;
+    %% Plot the fixation duration information
+    plot_fixDurs
     
-    
-    % Outcomes
-    currOutcome = p.trial.outcome.CurrOutcome;    
-    p.plotdata.outcome(nTrials, 1) = currOutcome;
-    allOutcomes = p.plotdata.outcome;
-    
-    iCorrect = allOutcomes == p.trial.outcome.Correct;
-    iBreak = allOutcomes == p.trial.outcome.FixBreak;
-    
-    % Fix Times
-    p.plotdata.fixDur(nTrials, 1) = p.trial.task.fixDur;
-    fixDurs = p.plotdata.fixDur;
-    
-    % Plot it    
-    plot(times(iCorrect),  fixDurs(iCorrect),  'o', 'MarkerSize', 6, ...
-        'MarkerEdgeColor', correct_col, 'MarkerFaceColor', correct_col);
-    hold on;
-    plot(times(iBreak), fixDurs(iBreak), 'o', 'MarkerSize', 6, ...
-        'MarkerEdgeColor', break_col, 'MarkerFaceColor', break_col);
-    
-%     if(Ntrials > 4)
-%         X = [ones(length(Tm),1) Tm(:)];
-%         cFit = X\RT(:);
-%         cFitln = X*cFit;
-%         
-%         plot(Tm,cFitln,'-g', 'LineWidth', 2);
-%     end
-    
-%     if(Ntrials > smPT)
-%         RTsm = smooth(Tm,RT,smPT/Ntrials,'rloess');
-%         plot(Tm,RTsm,'-r');
-%     end
-
-    ylabel('fixation duration [s]');
-    xlabel('trial time [min]');
-    ylim([0, max(fixDurs)]);
-    if nTrials > 1
-        xlim([0, max(times)]);
-    end
-    set(gca, 'TickDir', 'out');
-    hold off
-
-    %--------------------------------------------------%
     % If a new neuron has been flagged, don't draw any of the other figures
     if p.trial.RF.flag_new
         drawnow;
         return;
     end
-    %--------------------------------------------------%
-    %% Plot fixation time histogram
-    % Shows the histogram of fixation time
-    subplot(plotRows, plotCols, plotCols)
     
-    binWidth = 1;
-    ibw = 1 / binWidth;
+    %% Plot coarse tuning information
+    plot_coarseHeatmap
+    plot_temporalProfile
     
-    % Get the edges of the histogram bins
-    maxEdge = ceil(max(fixDurs)*ibw)/ibw;
-    edges = 0:ibw:maxEdge;
-    
-    % Plot the histogram
-    h = histogram(fixDurs,edges);
-    
-    % Set up x axis
-    xlim([0,maxEdge]);
-    xlabel('Fixation Duration [s]');
-    
-    % Add labels for each bar
-    vals = h.Values;
-    textVals = cellfun(@num2str, num2cell(vals), 'UniformOutput', false);
-    
-    binCenters = (edges(1:end-1) + edges(2:end)) / 2;
-    textHeight = 1.1 * max(vals);
-    heights = repmat(textHeight,length(binCenters),1);
-    ylim([0, 1.2 * max(vals)])
-    
-    text(binCenters,heights,textVals,'horizontalalignment','center')
-    
-    
-    
-    
-    %% Plot of coarse heatmap
-    % Shows where stimuli appeared before spiking activity in the coarse mapping process
-    subplot(plotRows, plotCols, [plotCols+[1,2], 2*plotCols+[1,2]])
-    
-    rfdef = p.trial.RF.coarse;
-    
-    xRange = rfdef.xRange;
-    yRange = rfdef.yRange;
-	
-    % Draw the data
-    imagesc(xRange, yRange, rfdef.heatmap);
-    
-    % Flip the y axis since imagesc does y reversed for some reason
-    set(gca,'YDir','normal')
-    colormap(gca,coarse_colmap)
-    
-    % Draw an x showing where the maxPos is
-    maxPos = rfdef.maxPos;
-    hold on;
-    plot(maxPos(1),maxPos(2), 'x', 'MarkerSize', 50, 'MarkerEdgeColor', 'r', 'MarkerFaceColor','r')
-    hold off;
-    
-    % Draw a rectangle showing where the fine placement will be
-    hold on;
-    extent = p.trial.stim.fine.extent;
-    rad = p.trial.stim.fine.radius;
-    rect = [maxPos - (extent + rad), 2*(extent + rad), 2*(extent + rad)];
-    rectangle('Position', rect, 'LineWidth', 2, 'EdgeColor', 'r', 'LineStyle', ':');
-    
-    axis square;
-    hold off;
-    
-    %--------------------------------------------------%
-    %% Plot of the coarse temporal profile
-    % Shows when stimuli appeared relative to spikes
-    subplot(plotRows, plotCols, 3*plotCols + [1:2])
-    hold off;
-    t = linspace(rfdef.temporalRange(1)*1000, rfdef.temporalRange(2)*1000, p.trial.RF.temporalRes);
-    plot(t, rfdef.maxTemporalProfile, 'r')
-    xlabel('Stim temporal profile relative to spikes [ms]')
-    ylabel('Proportion');
-    
-    % Display vertical lines where the fine mapping cutoffs will be
-    hold on;
-    h = vline(p.trial.RF.fine.temporalRange*1000,'k--');
-    set(h,'LineWidth',2);
-    hold off;
-    
-    %--------------------------------------------------%
+    %% Plot fine tuning information
     % Only plot fine plots if fine stage is active
     if strcmp(p.trial.stim.stage, 'fine')
+        plot_fineHeatmap
+        plot_orientationTuning
+    end
+    
+catch me
+    disp('Online plots failed')
+    warning(me.message)
+end
+
+%% Draw plots
+drawnow
+
+    function plot_fixDurs
+        %% Plot of fixation time
+        % Takes up the top row of the plot. Shows fixation time over the duration of the entire experiment
+        subplot(plotRows, plotCols, 1:plotCols-1)
+        
+        % Get and save relavent information
+        % Trial times relative to start of experiment
+        p.plotdata.TaskStart(nTrials) = p.trial.EV.TaskStart;
+        p.plotdata.times(nTrials) = (p.trial.EV.TaskStart - p.plotdata.TaskStart(1)) / 60;
+        times = p.plotdata.times;
+        
+        
+        % Outcomes
+        currOutcome = p.trial.outcome.CurrOutcome;
+        p.plotdata.outcome(nTrials, 1) = currOutcome;
+        allOutcomes = p.plotdata.outcome;
+        
+        iCorrect = allOutcomes == p.trial.outcome.Correct;
+        iBreak = allOutcomes == p.trial.outcome.FixBreak;
+        
+        % Fix Times
+        p.plotdata.fixDur(nTrials, 1) = p.trial.task.fixDur;
+        fixDurs = p.plotdata.fixDur;
+        
+        % Plot it
+        plot(times(iCorrect),  fixDurs(iCorrect),  'o', 'MarkerSize', 6, ...
+            'MarkerEdgeColor', correct_col, 'MarkerFaceColor', correct_col);
+        hold on;
+        plot(times(iBreak), fixDurs(iBreak), 'o', 'MarkerSize', 6, ...
+            'MarkerEdgeColor', break_col, 'MarkerFaceColor', break_col);
+        
+        %     if(Ntrials > 4)
+        %         X = [ones(length(Tm),1) Tm(:)];
+        %         cFit = X\RT(:);
+        %         cFitln = X*cFit;
+        %
+        %         plot(Tm,cFitln,'-g', 'LineWidth', 2);
+        %     end
+        
+        %     if(Ntrials > smPT)
+        %         RTsm = smooth(Tm,RT,smPT/Ntrials,'rloess');
+        %         plot(Tm,RTsm,'-r');
+        %     end
+        
+        ylabel('fixation duration [s]');
+        xlabel('trial time [min]');
+        ylim([0, max(fixDurs)]);
+        if nTrials > 1
+            xlim([0, max(times)]);
+        end
+        set(gca, 'TickDir', 'out');
+        hold off
+        
+        %% Plot fixation time histogram
+        % Shows the histogram of fixation time
+        subplot(plotRows, plotCols, plotCols)
+        
+        binWidth = 1;
+        ibw = 1 / binWidth;
+        
+        % Get the edges of the histogram bins
+        maxEdge = ceil(max(fixDurs)*ibw)/ibw;
+        edges = 0:ibw:maxEdge;
+        
+        % Plot the histogram
+        h = histogram(fixDurs,edges);
+        
+        % Set up x axis
+        xlim([0,maxEdge]);
+        xlabel('Fixation Duration [s]');
+        
+        % Add labels for each bar
+        vals = h.Values;
+        textVals = cellfun(@num2str, num2cell(vals), 'UniformOutput', false);
+        
+        binCenters = (edges(1:end-1) + edges(2:end)) / 2;
+        textHeight = 1.1 * max(vals);
+        heights = repmat(textHeight,length(binCenters),1);
+        ylim([0, 1.2 * max(vals)])
+        
+        text(binCenters,heights,textVals,'horizontalalignment','center')
+    end
+
+    function plot_coarseHeatmap
+        
+        %% Plot of coarse heatmap
+        % Shows where stimuli appeared before spiking activity in the coarse mapping process
+        subplot(plotRows, plotCols, [plotCols+[1,2], 2*plotCols+[1,2]])
+        
+        rfdef = p.trial.RF.coarse;
+        
+        xRange = rfdef.xRange;
+        yRange = rfdef.yRange;
+        
+        % Draw the data
+        coarsehm = imagesc(xRange, yRange, rfdef.heatmap);
+        
+        % Flip the y axis since imagesc does y reversed for some reason
+        set(gca,'YDir','normal')
+        colormap(gca,coarse_colmap)
+        
+        % Draw an x showing where the maxPos is
+        maxPos = rfdef.maxPos;
+        hold on;
+        plot(maxPos(1),maxPos(2), 'x', 'MarkerSize', 50, 'MarkerEdgeColor', 'r', 'MarkerFaceColor','r')
+        hold off;
+        
+        % Draw a rectangle showing where the fine placement will be
+        hold on;
+        extent = p.trial.stim.fine.extent;
+        rad = p.trial.stim.fine.radius;
+        rect = [maxPos - (extent + rad), 2*(extent + rad), 2*(extent + rad)];
+        rectangle('Position', rect, 'LineWidth', 2, 'EdgeColor', 'r', 'LineStyle', ':');
+        
+        axis square;
+        hold off;
+        
+        % Set up clicking on the heatmap to set where the point is
+        set(coarsehm, 'ButtonDownFcn', @coarseHM_click);
+        
+    end
+
+    function plot_temporalProfile
+        %% Plot of the coarse temporal profile
+        % Shows when stimuli appeared relative to spikes
+        rfdef = p.trial.RF.coarse;
+        
+        subplot(plotRows, plotCols, 3*plotCols + [1:2])
+        hold off;
+        t = linspace(rfdef.temporalRange(1)*1000, rfdef.temporalRange(2)*1000, p.trial.RF.temporalRes);
+        plot(t, rfdef.maxTemporalProfile, 'r')
+        xlabel('Stim temporal profile relative to spikes [ms]')
+        ylabel('Proportion');
+        
+        % Display vertical lines where the fine mapping cutoffs will be
+        hold on;
+        h = vline(p.trial.RF.fine.temporalRange*1000,'k--');
+        set(h,'LineWidth',2);
+        hold off;
+        
+        %% Plot of the fine temporal profile
+        % This will be plotted on top of the coarse profile
+        if strcmp(p.trial.stim.stage, 'fine')
+            hold on;
+            rfdef = p.trial.RF.fine;
+            t = linspace(rfdef.temporalRange(1)*1000, rfdef.temporalRange(2)*1000, p.trial.RF.temporalRes);
+            plot(t, rfdef.maxTemporalProfile,'b');
+            hold off;
+        end
+        
+    end
+
+    function plot_fineHeatmap
         %% Plot of fine heatmap
         % Shows where stimuli appeared before spiking activity in the fine mapping process
         subplot(plotRows, plotCols, [plotCols+[3,4],plotCols*2+[3,4]])
@@ -197,16 +237,13 @@ try
         plot(maxPos(1),maxPos(2), 'x', 'MarkerSize', 50, 'MarkerEdgeColor', 'r', 'MarkerFaceColor','r')
         hold off;
         
-        %% Plot of the fine temporal profile
-        % This will be plotted on top of the coarse profile
-        subplot(plotRows, plotCols, 3*plotCols + [1:2])
-        hold on;
-        t = linspace(rfdef.temporalRange(1)*1000, rfdef.temporalRange(2)*1000, p.trial.RF.temporalRes);
-        plot(t, rfdef.maxTemporalProfile,'b');
-        hold off;
-        
-        
+    end
+
+    function plot_orientationTuning
         %% Plot of the orientation tuning
+        rfdef = p.trial.RF.fine;
+        stimdef = p.trial.stim.fine;
+        
         subplot(plotRows, plotCols, 3*plotCols + [3:4])
         angles = stimdef.angle;
         tuning = rfdef.orientationTuning;
@@ -224,13 +261,5 @@ try
         
         
     end
-    
-    
-    
-catch me
-    disp('Online plots failed')
-    warning(me.message)
-end
 
-%% Draw plots
-drawnow
+end
