491606ef85639366d775f2eca4429dc002707caa
Nate Faber <nathaniel@njfaber.com>
2017-11-01 12:43:29 -0500

    NF: Finish sending signals. Not tested yet though

--- utils/ND_SendStimRecord.m
+++ utils/ND_SendStimRecord.m
@@ -8,21 +8,36 @@ if nStims > 0
     % Start the block
     pds.datapixx.strobe(p.trial.event.STIMPROP_BLOCK_ON);
     
-    for iStim=1:nStims
+    for iStim = 1:nStims
         
         % Indicate new stim info incoming
         pds.datapixx.strobe(p.trial.event.STIMPROP);
         
         % Get the array that holds the stim properties
         stimProps = stimRecord{iStim};
+        nProps = length(stimProps);
         
         % Send one signal for each property in the stimProps array
         % Convert floats to 16-bit words
-        
+        for iProp = 1:nProps
+            property = stimProps(iProp);
+            
+            % The first property is a class code for the type of stim
+            % Just transmit it directly
+            if iProp == 1
+                pds.datapixx.strobe(property);
+            
+            else
+                % Otherwise, convert it to the corresponding 16-bit integer representing the double
+                signal = typecast(property, 'uint16');
+                pds.datapixx.strobe(signal);
+            end
+                
+        end
         
     end
     
-    pds.datapixx.strobe(p.trial.event.STIMPROP_BLOCK_ON);
+    pds.datapixx.strobe(p.trial.event.STIMPROP_BLOCK_OFF);
     
 end
     
