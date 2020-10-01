03feb393c4198e14a438bee3f068c41a2b942035
NeuroDisney <none>
2020-10-01 13:55:12 -0400

    PercEqui Reference and Target appear vertically

--- tasks/PercEqui/PercEqui_taskdef.m
+++ tasks/PercEqui/PercEqui_taskdef.m
@@ -51,9 +51,9 @@ p.trial.stim.GRATING.radius = 0.75;  % radius of grating patch
 % p.trial.stim.EccLst =  3;
 %p.trial.stim.AngLst = [45, 0, -45]; 
 
-p.trial.stim.PosX = 1;
+p.trial.stim.PosX = 0;
 %p.trial.stim.PosY = datasample([-4, 4], 1);
-p.trial.stim.PosY = 5;
+p.trial.stim.PosY = 2.5;
 
 % grating contrast
 %cCtr = datasample([0.03, 0.06, 0.09, 0.12], 1);
