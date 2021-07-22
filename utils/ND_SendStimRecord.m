b03b2a021ca6ec3fae808a77e6870562dc28ce8b
verocg <69829339+verocg@users.noreply.github.com>
2021-07-22 19:36:28 -0400

    Update ND_SendStimRecord.m
    added lines 28-32 to account for potential x or y position value of zero (until create new 6 digit variable for x and y position) since without this code conversion if x or y is zero the value is lost in the event code stimProp list

--- utils/ND_SendStimRecord.m
+++ utils/ND_SendStimRecord.m
@@ -25,11 +25,16 @@ if nStims > 0
             if iProp == 1
                 pds.datapixx.strobe(property);
            
+           elseif property == 0 
+                % If the property is 0, transmit a special signal (since signals are interspersed with 0 strobes, it is lost
+                % otherwise)
+                pds.datapixx.strobe(p.trial.event.ZERO_CODE);
+                
             elseif iProp == 2
                 signalx = typecast(cast(round(property * 100), 'int16'), 'uint16');
                 %xpos = 24000 + signalx;
                 pds.datapixx.strobe(signalx);
-               
+                            
             elseif iProp == 3
                 signaly = typecast(cast(round(property * 100), 'int16'), 'uint16');
                 %ypos = 25000 + signaly;
