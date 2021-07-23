486d3e92bd6601511fe72c1365a90d3774beb82c
verocg <69829339+verocg@users.noreply.github.com>
2021-07-23 11:45:53 -0400

    Update DetectGrat_init.m
    updating Rig3 with parameters specified for grating presentation for Watson training that had been made while on the Dev branch

--- tasks/DetectGrat/DetectGrat_init.m
+++ tasks/DetectGrat/DetectGrat_init.m
@@ -66,12 +66,12 @@ p.defaultParameters.task.RandomEcc  = 1; % if 1, randomly change the grating ecc
 p.defaultParameters.task.RandomAng  = 1; % if 1, randomly change the grating angular position each trial
 
 % define random grating parameters for each session
-%p.defaultParameters.stim.PosYlst    = -10;  % range of possible positions on Y axis Corey Note: I think this variable is not functional 
-%p.defaultParameters.stim.PosXlst    =  10;  % range of possible positions on X axis Corey Note: I think this variable is not functional
+% p.defaultParameters.stim.PosYlst    = 0;  % range of possible positions on Y axis Corey Note: I think this variable is not functional 
+% p.defaultParameters.stim.PosXlst    =  3;  % range of possible positions on X axis Corey Note: I think this variable is not functional
 
 % define grid locations used by key selection
-p.defaultParameters.stim.EccLst = [2, 3, 4, 2, 3, 4, 2, 3, 4]; % if p.defaultParameters.task.RandomEcc = 0; these are the eccentricities
-p.defaultParameters.stim.AngLst = [45, 45, 45, 0, 0, 0, -45, -45, -45]; % if p.defaultParameters.task.RandomAng  = 0; these are the angles
+p.defaultParameters.stim.EccLst = [4]; % if p.defaultParameters.task.RandomEcc = 0; these are the eccentricities
+p.defaultParameters.stim.AngLst = [360]; % if p.defaultParameters.task.RandomAng  = 0; these are the angles
 
 [p.defaultParameters.stim.GridX, p.defaultParameters.stim.GridY] = ...
     pol2cart(p.defaultParameters.stim.AngLst, p.defaultParameters.stim.EccLst); % Corey Note: determines the target position based off EccLst and AngLst
@@ -80,10 +80,10 @@ p.defaultParameters.stim.AngLst = [45, 45, 45, 0, 0, 0, -45, -45, -45]; % if p.d
 cPos = randi(length(p.defaultParameters.stim.GridX));
 p.defaultParameters.stim.Ecc  = p.defaultParameters.stim.EccLst(cPos);
 p.defaultParameters.stim.Ang  = p.defaultParameters.stim.AngLst(cPos);
-p.defaultParameters.stim.PosY = p.defaultParameters.stim.GridX(cPos); %use this line when you want random X positions
-p.defaultParameters.stim.PosX = p.defaultParameters.stim.GridY(cPos); %use this line when you want random Y positions 
-%p.defaultParameters.stim.PosY = -10; % Corey Hack: to get hardcoded single position
-%p.defaultParameters.stim.PosX = -10; %Corey Hack: to get hardcoded single position
+%p.defaultParameters.stim.PosY = p.defaultParameters.stim.GridX(cPos); %use this line when you want random X positions
+%p.defaultParameters.stim.PosX = p.defaultParameters.stim.GridY(cPos); %use this line when you want random Y positions 
+p.defaultParameters.stim.PosY = [-4]; % Corey Hack: to get hardcoded single position
+p.defaultParameters.stim.PosX = [0]; %Corey Hack: to get hardcoded single position
 
 
 p.defaultParameters.stim.sFreqLst   = [2 3 4]; % spatial frequency as cycles per degree
@@ -103,5 +103,4 @@ p.defaultParameters.task.Response     = NaN;
 % maxTrialLength is used to pre-allocate memory at several initialization
 % steps. It specifies a duration in seconds.
 p.defaultParameters.pldaps.maxTrialLength = 15;
-
 
