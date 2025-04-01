9b4a4b8a38af9fc9cd5ea42f34a9c95a37d1b55e
verocg <69829339+verocg@users.noreply.github.com>
2025-04-01 17:37:39 -0400

    Create ViewScene_taskdef.m

---
+++ ViewScene_taskdef.m
@@ -0,0 +1,29 @@
+% Function to define task parameters
+function p = ViewScene_taskdef(p)
+
+    % Setting time window for fixation before trial marked as 'NoStart'
+    p.trial.task.Timing.WaitFix = 2;
+    % Selecting inter-trial interval (ITI)
+    p.trial.task.Timing.ITI = ND_GetITI(1.25, 1.75, [], [], 1, 0.10);
+    % Setting time-out(s) for incorrect response
+    p.trial.task.Timing.TimeOut = 1; 
+    % Duration offset (sec) for video at normal play rate
+    p.trial.task.durOffset = 0;
+
+    p.trial.task.stim.videoDir = '/home/rig2-user/Videos/HierarchyVideos';
+    videos = dir(p.trial.task.stim.videoDir);
+    p.trial.task.stim.videoNames = {videos(~[videos.isdir]).name};
+
+    p.trial.stim.VIDEO.sizeGain = 2.1;
+    p.trial.stim.VIDEO.fixWin   = 80;
+    p.trial.stim.VIDEO.playRate = 1;
+
+
+    % Setting properties for fixation point
+    p.trial.stim.FIXSPOT.type = 'rect';    
+    p.trial.stim.FIXSPOT.color = 'red';
+    p.trial.stim.FIXSPOT.size = 0.4;
+    p.trial.stim.FIXSPOT.fixWin = 1.5;
+
+    
+end
