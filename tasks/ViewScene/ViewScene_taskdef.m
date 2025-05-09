2a385f220ec14ea6a4f2bf21565b4ad54510d14d
rig1-user <john.amodeo316@gmail.com>
2025-05-09 13:08:50 -0400

    adding image presentation to ViewScene

--- tasks/ViewScene/ViewScene_taskdef.m
+++ tasks/ViewScene/ViewScene_taskdef.m
@@ -10,16 +10,10 @@ function p = ViewScene_taskdef(p)
     % Duration offset (sec) for video at normal play rate
     p.trial.task.durOffset = 0;
 
-<<<<<<< HEAD
     % Setting scene configs
     p.trial.task.stim.sceneType = 'image'; % options: 'video' or 'image'
     %p.trial.task.stim.sceneDir = '/home/rig1-user/MatlabFiles/Videos/HierarchyVideos';
     p.trial.task.stim.sceneDir = '/home/rig1-user/MatlabFiles/Images/macaque_faces';
-=======
-    p.trial.task.stim.videoDir = '/home/rig2-user/Videos/HierarchyVideos';
-    videos = dir(p.trial.task.stim.videoDir);
-    p.trial.task.stim.videoNames = {videos(~[videos.isdir]).name};
->>>>>>> 76286abd520102913990037e5c0aadd90d2f20a6
 
     scenes = dir(p.trial.task.stim.sceneDir);
     p.trial.task.stim.sceneNames = {scenes(~[scenes.isdir]).name};
@@ -33,7 +27,6 @@ function p = ViewScene_taskdef(p)
     p.trial.stim.VIDEO.sizeGain = 2.1;
     p.trial.stim.VIDEO.fixWin   = 80;
     p.trial.stim.VIDEO.playRate = 1;
-
 
     % Setting properties for fixation point
     p.trial.stim.FIXSPOT.type = 'rect';    
